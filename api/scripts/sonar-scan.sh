90cdd4b97a95918f86e47a3110f4e700b5de1f81
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2021-01-17 17:49:35 -0300

    Configurar CI

---
+++ api/scripts/sonar-scan.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Stage 1: Begin sonar scan"
+dotnet sonarscanner begin \
+    /k:$PROJECT_KEY \
+    /o:$SONAR_ORGANIZATION \
+    /d:sonar.host.url=$SONAR_URL \
+    /d:sonar.login=$SONAR_SECRET
+
+echo "Stage 2: Analize project"
+dotnet build
+dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=opencover | true
+
+echo "Stage 3: End sonar scan"
+dotnet sonarscanner end /d:sonar.login=$SONAR_SECRET
